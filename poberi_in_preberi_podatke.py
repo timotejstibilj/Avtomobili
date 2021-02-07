6d0cc2943da644ca5ff67679b93c34584233430f
Timotej Stibilj <timotej.stibilj@gmail.com>
2021-02-07 19:45:01 +0100

    popravek pobiranja podatkov

--- poberi_in_preberi_podatke.py
+++ poberi_in_preberi_podatke.py
@@ -2,15 +2,12 @@ import re
 import requests
 import orodja
 
-STEVILO_STRANI = 2
-INDEKS_OBLIKE = 11
-# TODO: povecaj maxitemscomputed al neki takega za json, tudi za csv? default je 5000
-
+STEVILO_STRANI = 21
 
 vzorec_oglasa = re.compile(
     r"<span>(?P<ime_avtomobila>\w{3}.*?)</span>\n\s*</div>(.*\n\s*)*?"
     r"<td class=\"w-75 pl-3\">(?P<leto_prve_registracije>.*?)</td>(.*\n\s*)*?"
-    r"<td class=\"pl-3\">(?P<prevozeni_kilometri>\d+\skm)</td>(.*\n\s*)*?"
+    r"<td class=\Dd-none d-md-block pl-3\"\>\S+\<\/td\>\D*?<td class=\"pl-3\">(?P<prevozeni_kilometri>\d+\skm)</td>(.*\n\s*)*?"
     r"<td class=\"pl-3\">(?P<vrsta_motorja>\D*)</td>(.*\n\s*)*?"
     r"<td class=\"pl-3 text-truncate\">(?P<vrsta_menjalnika>.*menjalnik)</td>(.*\n\s*)*?"
     r"<td class=\"pl-3 text-truncate\">\n\s*(?P<karakteristike_motorja>\d*.*\D)\n\s*</td>(.*\n\s*)*?"
@@ -70,32 +67,36 @@ def izloci_podatke_oglasa(blok):
     return oglas
 
 
-def oglasi_na_strani(oblika, stran):
+def oglasi_na_strani(stran):
     url = (
-        "https://www.avto.net/Ads/results.asp?znamka=&model=&modelID=&tip=&znamka2=&model2=&tip2=&znamka3=&model3=&tip3=&"
-        f"cenaMin=0&cenaMax=999999&letnikMin=0&letnikMax=2090&bencin=0&starost2=999&oblika={oblika}&ccmMin=0&ccmMax=99999&"
-        "mocMin=&mocMax=&kmMin=0&kmMax=9999999&kwMin=0&kwMax=999&motortakt=0&motorvalji=0&lokacija=0&sirina=0&dolzina=&"
-        "dolzinaMIN=0&dolzinaMAX=100&nosilnostMIN=0&nosilnostMAX=999999&lezisc=&presek=0&premer=0&col=0&vijakov=0&EToznaka=0&"
-        "vozilo=&airbag=&barva=&barvaint=&EQ1=1000000000&EQ2=1000000000&EQ3=1000000000&EQ4=100000000&EQ5=1000000000&EQ6="
-        "1000000000&EQ7=1000100020&EQ8=1010000001&EQ9=1000000000&KAT=1010000000&PIA=&PIAzero=&PSLO=&akcija=0&paketgarancije=&"
-        f"broker=0&prikazkategorije=0&kategorija=0&zaloga=10&arhiv=0&presort=3&tipsort=DESC&stran={stran}"
+        "https://www.avto.net/Ads/results.asp?znamka=&model=&modelID=&tip=&znamka2="
+        "&model2=&tip2=&znamka3=&model3=&tip3=&cenaMin=0&cenaMax=999999&letnikMin=0"
+        "&letnikMax=2090&bencin=0&starost2=999&oblika=&ccmMin=0&ccmMax=99999&mocMin"
+        "=&mocMax=&kmMin=5000&kmMax=9999999&kwMin=0&kwMax=999&motortakt=&motorvalji"
+        "=&lokacija=0&sirina=&dolzina=&dolzinaMIN=&dolzinaMAX=&nosilnostMIN=&nosiln"
+        "ostMAX=&lezisc=&presek=&premer=&col=&vijakov=&EToznaka=&vozilo=&airbag=&bar"
+        "va=&barvaint=&EQ1=1000000000&EQ2=1000000000&EQ3=1000000000&EQ4=100000000&EQ"
+        "5=1000000000&EQ6=1000000000&EQ7=1000100020&EQ8=1010000001&EQ9=100000000&KAT"
+        "=1010000000&PIA=&PIAzero=&PSLO=&akcija=&paketgarancije=0&broker=&prikazkat"
+        f"egorije=&kategorija=&ONLvid=&ONLnak=&zaloga=10&arhiv=&presort=&tipsort=&stran={stran}"
     )
-    ime_datoteke = f"rabljeni-avtomobili-oblika-{oblika}-stran-{stran}.html"
+    ime_datoteke = f"rabljeni-avtomobili-stran-{stran}.html"
     orodja.shrani_spletno_stran(url, ime_datoteke)
 
 
 def zapisi_oglase():
     oglasi = []
     for stran in range(1, STEVILO_STRANI + 1):
-        for oblika in range(INDEKS_OBLIKE, INDEKS_OBLIKE + 6):
-            # shrani spletne strani
-            oglasi_na_strani(oblika, stran)
+        # shrani spletne strani
+        oglasi_na_strani(stran)
 
-            ime_datoteke = f"rabljeni-avtomobili-oblika-{oblika}-stran-{stran}.html"
-            vsebina = orodja.vsebina_datoteke(ime_datoteke)
-            for oglas in vzorec_oglasa.finditer(vsebina):
-                urejeni_podatki = izloci_podatke_oglasa(oglas.groupdict())
-                oglasi.append(urejeni_podatki)
+        ime_datoteke = f"rabljeni-avtomobili-stran-{stran}.html"
+        vsebina = orodja.vsebina_datoteke(ime_datoteke)
+        for oglas in vzorec_oglasa.finditer(vsebina):
+            urejeni_podatki = izloci_podatke_oglasa(oglas.groupdict())
+            oglasi.append(urejeni_podatki)
+
+        print(f"končana je stran {stran}")
 
     oglasi.sort(key=lambda oglas: oglas["ime_avtomobila"])
     return oglasi
